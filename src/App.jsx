384c55726c8fd8bd0a3ba75dad2bbaf157d4ff73
Fadhlan Syahran Jungjungan <fadhlan.areksmp@gmail.com>
2024-06-12 18:15:57 +0700

    Change Project Type

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider, Route, Outlet } from 'react-router-dom'
+import Footer from './Components/FooterDiv/Footer'
+import NavBar from './Components/NavBar/NavBar'
+import Home from './Pages/Home'
+import Jobs from './Pages/Jobs'
+import JobSingle from './Pages/JobSingle'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
+
+const Layout = () => {
+  return(
+    <>
+    <Footer/>
+    <Outlet />
+    <NavBar />
+    </>
+    
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <RouterProvider router={route}/>
+    </div>
+  )
+}
+
+const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/jobs/:id",
+        element: <JobSingle />
+      },
+      {
+        path: "/jobs",
+        element: <Jobs />
+      },
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  }
+])
+
+export default App
---
+++ src/Pages/JobSingle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const JobSingle = () => {
+  return (
+    <div>JobSingle</div>
+  )
+}
+
+export default JobSingle
